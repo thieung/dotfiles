5d5206ef1389c9201bf42d042794c381f83c21ca
Thieu Nguyen <thieunguyen.tech@gmail.com>
2024-07-15 11:17:10 +0700

    (feat) hammerspoon: add config

--- dot_config/hammerspoon/init.lua
+++ dot_config/hammerspoon/init.lua
@@ -2,30 +2,54 @@
 Hyper = { "cmd", "alt", "ctrl" }
 
 local notify = function()
-  hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
+	hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
 end
 
 -- Use 0 to reload the configuration
 hs.hotkey.bind(Hyper, "0", function()
-  notify()
-  hs.reload()
+	notify()
+	hs.reload()
 end)
 
 local function reload_config(files)
-  local doReload = false
-  for _, file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    notify()
-    hs.reload()
-  end
+	local doReload = false
+	for _, file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		notify()
+		hs.reload()
+	end
 end
 
 -- reload the config every time it changes
-hs.pathwatcher.new(os.getenv("HOME") .. "/.dotfiles/.config/hammerspoon/", reload_config):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "/.config/hammerspoon/", reload_config):start()
+
+------------------------------------ SKETCHYBAR --------------------------------
+-- load sketchybar_lua
+package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
+local sbar = require("sketchybar")
+
+-- Create a window filter to monitor windows only on the external monitor
+local wf = hs.window.filter.new(function(win)
+	return win:isVisible() and not win:isFullScreen()
+end)
+wf:subscribe(hs.window.filter.windowTitleChanged, function(win)
+	if win then
+		sbar.trigger("title_change", {
+			title = win:title(),
+		})
+	end
+end)
+wf:subscribe(hs.window.filter.windowFocused, function(win)
+	if win then
+		sbar.trigger("window_focus", {
+			title = win:title(),
+		})
+	end
+end)
 
 ------------------------------------ MODULES -----------------------------------
 require("keymaps")
--- dot_config/hammerspoon/keymaps.lua
+++ dot_config/hammerspoon/keymaps.lua
@@ -7,53 +7,51 @@ local hyper = Hyper
   this, a table can be passed which contains the app name followed by the filename
 ]]
 local apps = {
-  b = "Safari", -- Browser
-  c = "ChatGPT",
-  d = "Discord",
-  e = "Microsoft Excel",
-  f = "Finder",
-  g = "Google Chrome",
-  m = "HEY", -- Mail
-  n = "Bear", -- Notes
-  o = "Notion", -- Life OS
-  p = "1Password",
-  -- r = RESERVED
-  t = "WezTerm", -- Terminal
-  v = "VLC", -- Video
-  w = "word",
-  z = "Todoist",
+	b = "Safari", -- Browser
+	c = "ChatGPT",
+	d = "Discord",
+	e = "Microsoft Excel",
+	f = "Finder",
+	g = "Google Chrome",
+	m = "Microsoft Outlook", -- Mail
+	n = "Bear", -- Notes
+	o = "Notion",
+	p = "Bitwarden",
+	t = "kitty", -- Terminal
+	v = "VLC", -- Video
+	w = "Microsoft Word",
 }
 
 local LaunchOrToggle = function(key, app_name, app_filename)
-  hs.hotkey.bind(hyper, key, function()
-    local app = hs.application.find(app_name)
-    -- Toggle - show
-    local awin = nil
-    if app then
-      awin = app:mainWindow()
-    end
-    -- Toggle - hide
-    if awin and app and app:isFrontmost() then
-      app:hide()
-    else
-      -- Launch
-      if app_filename then
-        return hs.application.launchOrFocus(app_filename)
-      end
+	hs.hotkey.bind(hyper, key, function()
+		local app = hs.application.find(app_name)
+		-- Toggle - show
+		local awin = nil
+		if app then
+			awin = app:mainWindow()
+		end
+		-- Toggle - hide
+		if awin and app and app:isFrontmost() then
+			app:hide()
+		else
+			-- Launch
+			if app_filename then
+				return hs.application.launchOrFocus(app_filename)
+			end
 
-      app = hs.application.find(app_name)
+			app = hs.application.find(app_name)
 
-      hs.application.launchOrFocus(app_name)
-      app.setFrontmost(app)
-      app.activate(app)
-    end
-  end)
+			hs.application.launchOrFocus(app_name)
+			app.setFrontmost(app)
+			app.activate(app)
+		end
+	end)
 end
 
 for key, app_name in pairs(apps) do
-  if type(app_name) == "table" then
-    LaunchOrToggle(key, app_name[1], app_name[2])
-  else
-    LaunchOrToggle(key, app_name)
-  end
+	if type(app_name) == "table" then
+		LaunchOrToggle(key, app_name[1], app_name[2])
+	else
+		LaunchOrToggle(key, app_name)
+	end
 end

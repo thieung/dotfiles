35a48168e4feb2b8966422fb4f921960e74e25a5
Thieu Nguyen <thieunguyen.tech@gmail.com>
2024-09-23 22:48:00 +0700

    chore(nvim): update config

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -182,25 +182,36 @@ return {
           ["+"] = { "g<C-a>", desc = "Increment number" },
           ["-"] = { "g<C-x>", desc = "Descrement number" },
         },
+        ia = vim.fn.has "nvim-0.10" == 1 and {
+          mktmpl = { function() return "<++>" end, desc = "Insert <++>", expr = true },
+          ldate = { function() return os.date "%Y/%m/%d %H:%M:%S -" end, desc = "Y/m/d H:M:S -", expr = true },
+          ndate = { function() return os.date "%Y-%m-%d" end, desc = "Y-m-d", expr = true },
+          xdate = { function() return os.date "%m/%d/%y" end, desc = "m/d/y", expr = true },
+          fdate = { function() return os.date "%B %d, %Y" end, desc = "B d, Y", expr = true },
+          Xdate = { function() return os.date "%H:%M" end, desc = "H:M", expr = true },
+          Fdate = { function() return os.date "%H:%M:%S" end, desc = "H:M:S", expr = true },
+        } or nil,
       },
-    }) --[[@as AstroCoreOpts]]
+    } --[[@as AstroCoreOpts]]) --[[@as AstroCoreOpts]]
+
+    local maps = opts.mappings
+    ---@cast maps -nil
 
     -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-    for lhs, rhs in pairs {
-      n = { "'Nn'[v:searchforward]", expr = true, desc = "Next Search Result" },
-      N = { "'nN'[v:searchforward]", expr = true, desc = "Prev Search Result" },
-    } do
-      opts.mappings.n[lhs] = rhs
-      opts.mappings.x[lhs] = rhs
-      opts.mappings.o[lhs] = rhs
-    end
+    maps.n.n = { "'Nn'[v:searchforward].'zv'", expr = true, desc = "Next Search Result" }
+    maps.x.n = { "'Nn'[v:searchforward]", expr = true, desc = "Next Search Result" }
+    maps.o.n = { "'Nn'[v:searchforward]", expr = true, desc = "Next Search Result" }
+    maps.n.N = { "'nN'[v:searchforward].'zv'", expr = true, desc = "Prev Search Result" }
+    maps.x.N = { "'nN'[v:searchforward]", expr = true, desc = "Prev Search Result" }
+    maps.o.N = { "'nN'[v:searchforward]", expr = true, desc = "Prev Search Result" }
 
     -- add line text object
     for lhs, rhs in pairs {
       il = { ":<C-u>normal! $v^<CR>", desc = "inside line" },
       al = { ":<C-u>normal! V<CR>", desc = "around line" },
     } do
-      opts.mappings.x[lhs] = rhs
+      maps.o[lhs] = rhs
+      maps.x[lhs] = rhs
     end
 
     -- add missing in between and around two character pairs
@@ -209,7 +220,8 @@ return {
         ["i" .. char] = { (":<C-u>silent! normal! f%sF%slvt%s<CR>"):format(char, char, char), desc = "inside " .. char },
         ["a" .. char] = { (":<C-u>silent! normal! f%sF%svf%s<CR>"):format(char, char, char), desc = "around " .. char },
       } do
-        opts.mappings.x[lhs] = rhs
+        maps.o[lhs] = rhs
+        maps.x[lhs] = rhs
       end
     end
 
